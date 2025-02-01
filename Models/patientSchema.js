695c081169fec37f540dfa0501d93c1f7ca7f278
Gaurav Phule <gaurav230605@gmail.com>
2025-02-01 14:52:10 +0530

    Patient Schema created

---
+++ Models/patientSchema.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose")
+
+const patientModel = new mongoose.Schema({
+  patientName: { type: String, required: true, },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  dob: { type: Date, required: true },
+  phone: { type: String, required: true },
+  gender: { type: String, enum: ["Male", "Female", "Others"], required: true },
+  role: { type: String, enum: ["Patient"], default: "Patient" },
+  bookings: [{
+    doctorID:{type:mongoose.Schema.Types.ObjectId, ref:"Doctor"},
+    bookingDate:{type:Date},
+    bookingSlot:{tyoe:String},
+    isApproved:{type:String, enum:["Pending","Confirm","Rejected"], default:"Pending"}
+  }]
+});
+
+const Patient = mongoose.model("Patient",patientModel);
+
+module.exports = {
+  Patient,
+}
