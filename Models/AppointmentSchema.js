9845844a3b9ca1bd93a6d5532e4c35ba2ce223c3
Gaurav Phule <gaurav230605@gmail.com>
2025-03-17 00:08:37 +0530

    Updation of status handled leaving cancled status

--- Models/AppointmentSchema.js
+++ Models/AppointmentSchema.js
@@ -1,4 +1,27 @@
 const mongoose = require("mongoose")
+const Cancled = require("./cancledSchema")
+const Completed = require("./completedSchema")
+const Pending = require("./pendingSchema")
+const Approved = require("./approvedSchema")
+
+
+async function forApproved(appointment1){
+  const rm = Pending.findOneAndDelete({appointment:appointment1._id})
+
+  const approved = await Approved.create({
+    appointment:appointment1._id
+  })
+  console.log("removed from pending ", rm, " approved ", approved)
+}
+async function forCompleted(appointment1){
+  const rm = Approved.findOneAndDelete({appointment:appointment1._id})
+
+  const complete = await Completed.create({
+    appointment:appointment1._id
+  })
+  console.log("removed from approved ", rm, " completed ", complete)
+}
+
 
 const appointmentSchema = new mongoose.Schema({
   patient: {type:mongoose.Schema.Types.ObjectId, ref:"Patient", required:true},
@@ -9,9 +32,22 @@ const appointmentSchema = new mongoose.Schema({
   status: {type:String, enum:["Pending","Approved","Completed","Cancled"], default:"Pending"},
 
 },{timestamps:true})
-// appointmentSchema.post("save", async function (next){
-
-// })
+appointmentSchema.pre("save", async function (next){
+  const appointment = this
+  if(appointment.isModified("status")){
+    switch (appointment.status) {
+      case "Approved":
+        forApproved(appointment)
+        break;
+      case "Completed":
+        forCompleted(appointment)
+        break;
+      default:
+        console.log("no change")
+        break;
+    }
+  }
+})
 
 const Appointment = mongoose.model("Appointment",appointmentSchema)
 
