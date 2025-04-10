7717c1d8eb69054731fb444f727ca04df611f299
Gaurav Phule <gaurav230605@gmail.com>
2025-04-10 22:04:21 +0530

    Remove redundant Access-Control-Allow-Credentials header setting in CORS middleware

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use(cors({
 }));
 app.use((req, res, next) => {
   console.log("aftr cors");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
+
   next();
 });
 // ✅ Essential Middlewares
